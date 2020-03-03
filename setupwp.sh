2b60c41e4d68d05102ee20212cd71581bfb650c7
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-03 14:16:53 -0500

    Update setupwp.sh

--- setupwp.sh
+++ setupwp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apk update && apk upgrade && apk autoremove 
 ###
-apk add php7-common php7-iconv php7-json php7-gd php7-curl php7-xml php7-mysqli php7-imap php7-cgi fcgi php7-pdo php7-pdo_mysql php7-soap php7-xmlrpc php7-posix php7-mcrypt php7-gettext php7-ldap php7-ctype php7-dom php-phar php7-sqlite3 php7-pdo_sqlite
+apk add php7-iconv php7-json php7-gd php7-curl php7-xml php7-mysqli php7-imap php7-cgi fcgi php7-pdo php7-pdo_mysql php7-soap php7-xmlrpc php7-posix php7-mcrypt php7-gettext php7-ldap php7-ctype php7-dom php-phar php7-sqlite3 php7-pdo_sqlite
 ###
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 ###
